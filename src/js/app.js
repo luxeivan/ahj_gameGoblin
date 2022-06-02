1a215a8027e0241d6954d60dfd164ff87f6c8ccf
luxeivan <31414039+luxeivan@users.noreply.github.com>
2022-06-02 09:56:56 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import img from '../img/goblin.png';
+
+const gridElem = document.querySelectorAll('.grid__elem');
+const image = document.createElement('img');
+let currentNumberElem = 0;
+const arrayElem = [...gridElem];
+image.className = 'image';
+image.src = img;
+
+function generateNumberElem() {
+  const min = 0;
+  const max = 15;
+  const numberElem = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (currentNumberElem !== numberElem) {
+    currentNumberElem = numberElem;
+  } else {
+    generateNumberElem();
+  }
+}
+
+setInterval(() => {
+  generateNumberElem();
+  arrayElem[currentNumberElem].insertBefore(image, null);
+}, 1000);
